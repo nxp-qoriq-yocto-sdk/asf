ae72e066904c9b4093464a01b4a30b49985d479a
Ganga Negi <ganga.negi@freescale.com>
2014-08-20 20:59:40 +0000

    ASF IPv6 Firewall: Sending high rate traffic in the
    beginning gives crash.
    
    While IPv6 firewall forwarding at high rate in the beginning, network device
    pointer of flow was NULL because L2Blob didn't not get resolved. Hence, it was
    giving crash on accessing device pointer's member 'flags' to know device UP or
    DOWN status.
    
    Resolved by putting check whether network device is NULL or not.
    
    Signed-off-by: Ganga Negi <ganga.negi@freescale.com>
    Change-Id: Iab2daab86f830593bfa705d784361bd21951dbc5
    CR:ENGR00330131
    Reviewed-on: http://git.am.freescale.net:8181/18097
    Reviewed-by: Sandeep Malik <Sandeep.Malik@freescale.com>
    Tested-by: Sandeep Malik <Sandeep.Malik@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/22640
    Reviewed-on: http://git.am.freescale.net:8181/29940

--- asfmodule/asfffp/driver/asfipv6core.c
+++ asfmodule/asfffp/driver/asfipv6core.c
@@ -1451,7 +1451,7 @@ ASF_uint32_t ASFFFPIPv6ProcessAndSendPkt(
 				goto gen_indications;
 			}
 
-			if (!(flow->odev->flags & IFF_UP)) {
+			if ((flow->odev) && !(flow->odev->flags & IFF_UP)) {
 				L2blobRefresh = ASF_L2BLOB_REFRESH_DROP_PKT;
 				goto gen_indications;
 			}
